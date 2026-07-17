9f1011fb1abafd9fd55f7d49e98df5d82f06b2b8
Andrea Nisticò <andrea.nistic@gmail.com>
2026-07-17 14:27:46 +0200

    update

--- private_dot_config/nvim/lua/config/keymaps.lua
+++ private_dot_config/nvim/lua/config/keymaps.lua
@@ -2,11 +2,16 @@
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
 
-local function black_hole_delete()
-  return vim.v.register == '"' and '"_d' or "d"
+local function black_hole_delete(key)
+  return vim.v.register == '"' and '"_' .. key or key
 end
 
-vim.keymap.set({ "n", "x" }, "d", black_hole_delete, { expr = true, desc = "Delete without yanking" })
+for _, key in ipairs({ "d", "D", "x", "X" }) do
+  vim.keymap.set({ "n", "x" }, key, function()
+    return black_hole_delete(key)
+  end, { expr = true, desc = "Delete without yanking" })
+end
+
 vim.keymap.set("n", "dd", function()
-  return vim.v.register == '"' and '"_dd' or "dd"
+  return black_hole_delete("dd")
 end, { expr = true, desc = "Delete line without yanking" })
