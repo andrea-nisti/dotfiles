5f359c92759d2797ffdd8f46260ad36186fc3567
Andrea Nisticò <andrea.nistic@gmail.com>
2026-07-17 12:38:07 +0200

    update

--- private_dot_config/nvim/lua/config/keymaps.lua
+++ private_dot_config/nvim/lua/config/keymaps.lua
@@ -1,3 +1,12 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+
+local function black_hole_delete()
+  return vim.v.register == '"' and '"_d' or "d"
+end
+
+vim.keymap.set({ "n", "x" }, "d", black_hole_delete, { expr = true, desc = "Delete without yanking" })
+vim.keymap.set("n", "dd", function()
+  return vim.v.register == '"' and '"_dd' or "dd"
+end, { expr = true, desc = "Delete line without yanking" })
